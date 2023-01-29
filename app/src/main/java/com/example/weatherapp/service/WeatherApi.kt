8bccffc028ff42c091b89e1dfe15d7e98b034249
alp cetinkus <alpcetinkus@gmail.com>
2023-01-29 18:30:21 +0300

    change default location from api

--- app/src/main/java/com/example/weatherapp/service/WeatherApi.kt
+++ app/src/main/java/com/example/weatherapp/service/WeatherApi.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 
 interface WeatherApi {
 //https://api.openweathermap.org/data/2.5/weather?q=marmaris&APPID=75faa8083541a6689f4998dab66234a4
-    @GET("data/2.5/weather?q=marmaris&APPID=75faa8083541a6689f4998dab66234a4")
+    @GET("data/2.5/weather?q=maltepe&APPID=75faa8083541a6689f4998dab66234a4")
     fun getData(): Call<WeatherModel>
 }
